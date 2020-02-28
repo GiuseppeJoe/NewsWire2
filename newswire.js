a9a203838d7061fd19429cc3c44b1ac15cd72717
GiuseppeJoe <61272993+GiuseppeJoe@users.noreply.github.com>
2020-02-28 22:27:00 +1030

    Update newswire.js

--- newswire.js
+++ newswire.js
@@ -13,8 +13,8 @@ fs.readFile(newsDir, 'utf8', (err, jsonString) => {
     articles = jsonString ? JSON.parse(jsonString) : {};
 });
 
-    let newswire = require('./newswire');
-    let latestNews = new newswire(type, 'https://discordapp.com/api/webhooks/682906601191243776/-2oQOfUUeuZoh_MBXN2puFvjUrYeXAFK2smfUuZ4mfVJcXdMs3kX5ZuW535zXdb_sG4_');
+let newswire = require('./newswire');
+let latestNews = new newswire(type, 'https://discordapp.com/api/webhooks/682906601191243776/-2oQOfUUeuZoh_MBXN2puFvjUrYeXAFK2smfUuZ4mfVJcXdMs3kX5ZuW535zXdb_sG4_');
 
 
 class newswire {
